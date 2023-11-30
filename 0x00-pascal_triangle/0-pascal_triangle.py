d91efed31266bbc38cf50997d117ec722681a4ee
cheronodaisy <jepchumbadaisy96@gmail.com>
2023-11-30 15:17:22 +0000

    Update solution

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,23 @@
-def pascal_triangle(n):
-    if n <= 0:
-        return []
-    
-    triangle = [[1]]
-    
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
-        for j in range(1, len(prev_row)):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-        new_row.append(1)
-        triangle.append(new_row)
-    
-    return triangle
+#!/usr/bin/python3
+'''Module to return pascal triangle'''
 
+
+def pascal_triangle(n):
+    '''
+    Pascal's triangle
+    Args:
+      n (int): The number of rows of the triangle
+    Returns:
+      List of lists of integers representing the Pascal’s triangle
+    '''
+    lists = []
+    if n == 0:
+        return lists
+    for i in range(n):
+        lists.append([])
+        lists[i].append(1)
+        if (i > 0):
+            for j in range(1, i):
+                lists[i].append(lists[i - 1][j - 1] + lists[i - 1][j])
+            lists[i].append(1)
+    return lists
